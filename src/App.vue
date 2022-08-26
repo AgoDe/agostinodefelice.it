7d99042003fdb39bbcd0e2cdfff7c49077583853
Agostino De Felice <agostino@parkingmycar.it>
2022-08-26 12:45:39 +0200

    creata section con intro e titolo

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,112 @@
 <template>
   <div id="app">
 
-    <TitleContainer/>
+      
+    <section class="all_display init_section">
+
+        <div class="container h-100">
+          <div class="row align-items-center h-100 ">
+            <div class="col-12 text-center init_box">
+
+              <h3 class="init_an">scroll to start</h3>
+
+              <div class=" init_an chevron_box">
+                <i class="icon-pixel-down"></i>
+                <i class="icon-pixel-down mt-3"></i>
+              </div>
+
+              <h1 class="title_an">agostino de felice</h1>
+              <h3 class="title_an">full stack web developer</h3>
+
+              <div class="second chevron_box">
+                  <i class="icon-pixel-down"></i>
+                  <i class="icon-pixel-down mt-3"></i>
+              </div>
+              
+            </div>
+
+          </div>
+
+        </div>
+
+      </section>
+
   
   </div>
 </template>
 
 <script>
-import TitleContainer from "./components/titleContainer.vue";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 
 export default {
   name: 'App',
   components: {
-    TitleContainer
-}
+    
+  },
+  data(){
+    return{
+
+    }
+  }, // end of data
+  mounted(){
+
+    this.initAnimation();
+  }, // end of mounted
+  methods: {
+    initAnimation: function(){
+      let initTl = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.init_section',
+          end: '+=700',
+          scrub: 5,
+          markers: true,
+          pin: '.init_section',
+        },
+        ease: "power4.out"
+      });
+
+      initTl
+      .to('.init_an', { y: () => - window.innerHeight})
+      .from('.title_an', {y: () => window.innerHeight, opacity: 0, stagger: 0.2})
+      .to('.init_box', {border: '5px solid #eac817', duration: 2})
+      .from('.second.chevron_box', {y: () => window.innerHeight, opacity: 0, delay: 0.3})
+
+    }
+  }, // end of methods
+
 }
 </script>
 
 <style lang="scss">
 @import 'src/styles/main.scss';
  
+ .init_section {
+
+  .init_box {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    height: 60%;
+    border: 5px solid transparent;
+    border-radius: 15px;
+
+    h1.title_an {
+      font-size: 35px;
+      letter-spacing: 8px;
+      opacity: 1;
+      margin-bottom: 30px;
+
+    }
+    h3.title_an {
+      color: white;
+      font-size: 20px;
+      letter-spacing: 2px;
+      opacity: 1;
+    }
+  }
+
+ }
 </style>
--- src/components/titleContainer.vue
+++ src/components/titleContainer.vue
@@ -1,15 +1,15 @@
 <template>
-<div>
+<div class="">
 
     <section class="all_display welcome_container">
 
         <div class="container h-100">
             <div class="row h-100 align-items-center">
 
-                <div class="col-12 text-center">
-                    <h3>scroll to start</h3>
-                    <i class="icon-pixel-down"></i>
-                    <i class="icon-pixel-down mt-3"></i>
+                <div class="col-12 text-center welcome_box">
+                    <h3 class=" welcome_animation">scroll to start</h3>
+                    <i class="icon-pixel-down welcome_animation"></i>
+                    <i class="icon-pixel-down mt-3 welcome_animation"></i>
                 </div>
                 
             </div>
@@ -20,24 +20,162 @@
 
     <section class="all_display title_container">
 
+        <div class="pacman_box_in">
+            
+            <div class="ghost"></div>
+
+            <div class="pacman">
+                <div class="mouth_dx"></div>
+            </div>
+        </div>
+
+        <div class="container h-100">
+
+            <div class="row h-100 align-items-center">
+                <div class="col-12 title_box text-center">
+                    <h1>agostino de felice</h1>
+                    <h3>full stack web developer</h3>
+                </div>
+             
+            </div>
+        </div>
+
+          <div class="pacman_box_out">
+            
+            <div class="vulnerable_ghost"></div>
+
+            <div class="pacman">
+                <div class="mouth_sx"></div>
+            </div>
+        </div>
+
+
     </section>
     <!-- end of title_container -->
 </div>
 </template>
 
 <script>
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 
 export default {
     data(){
         return {
-
+            PacmanVel: 650,
         }
     }, // end of data
     mounted(){
 
+        this.pacmanAnimation();
+
     }, // end of mounted
     methods: {
+        pacmanAnimation: function() {
+            console.log(window.innerWidth)
 
+            // pacman in entrata
+            gsap.to('.prova', {
+                
+                scrollTrigger: {
+                    trigger: '.prova_box',
+                    markers: false,
+                    toggleActions: 'restart pause resume resume',
+                    
+                },
+                x: () => window.innerWidth + 200,
+                duration: window.innerWidth / this.PacmanVel,
+                ease: 'linear',
+                
+            })
+
+            // pacman in uscita
+
+            gsap.to('.pacman_box_out', {
+                
+                scrollTrigger: {
+                    trigger: '.title_container',
+                    start: 'bottom 80%',
+                    end: 'bottom top',
+                    markers: false,
+                    toggleActions: 'restart pause resume resume',
+                    
+                },
+                x: () => - window.innerWidth -300,
+                duration: window.innerWidth / this.PacmanVel,
+                ease: 'linear',
+                
+            })
+
+            // animazione titoli in entrata
+            gsap.fromTo('.title_box h1',
+                {
+                    x: () => window.innerWidth,
+                }, 
+                {
+                    scrollTrigger: {
+                        scrub: 5,
+                        trigger: '.title_container',
+                        start: 'top 65%',
+                        end: 'center center',
+                        markers: false,
+                        id: 'ingresso',
+                    },
+                    x: 0, 
+                })
+            gsap.fromTo('.title_box h3',
+                {
+                    
+                    x: () => - window.innerWidth,
+                }, 
+                {
+                    scrollTrigger: {
+                        scrub: 5,
+                        trigger: '.title_box',
+                        end: '+=600',
+                    },
+                    x: 0,
+                })
+
+            // animazioni titoli in uscita
+            gsap.fromTo('.title_box h1', 
+            {
+                x: 0
+            },
+            {
+                scrollTrigger: {
+                        scrub: 3,
+                        trigger: '.title_container',
+                        start: 'bottom 80%',
+                        end: 'bottom top',
+                        markers: false,
+                        id: 'uscita',
+                    },
+                x: () => - window.innerWidth - 100,
+                
+            })
+            gsap.fromTo('.title_box h3', 
+            {
+                x: 0
+            },
+            {
+                scrollTrigger: {
+                        scrub: 3,
+                        trigger: '.title_container',
+                        start: 'bottom 80%',
+                        end: 'bottom top',
+                        markers: false,
+                        id: 'uscita',
+                    },
+                x: () => window.innerWidth + 100,
+                
+            })
+          
+          
+        }
     }, // end of methods
 
 }
@@ -50,7 +188,7 @@ export default {
         
         .col-12 {
             position: relative;   
-               
+            
             h3 {
                 text-transform:capitalize;
             }
@@ -64,6 +202,50 @@ export default {
             }
         }
     } // end of welcome_container
+
+    .title_container {
+        position: relative;
+        .pacman_box_in {
+            position: absolute;
+            top: 200px;
+            left: -200px;
+           
+            .pacman {
+                position: absolute;
+                top: 0;
+                margin-left: 100px;
+            }
+        }
+
+        .title_box {
+            h1 {
+                font-size: 30px;
+                text-transform: capitalize;
+                margin-bottom: 30px;
+                letter-spacing: 5px;
+            }   
+            h3 {
+                color: white;
+                font-size: 20px;
+                text-transform: capitalize;
+                letter-spacing: 2px;
+
+            }
+        }
+
+         .pacman_box_out {
+            position: absolute;
+            bottom: 200px;
+            right: -200px;
+           
+            .pacman {
+                position: absolute;
+                top: 0;
+                margin-left: 100px;
+            }
+        }
+        
+    }
 
 
 
